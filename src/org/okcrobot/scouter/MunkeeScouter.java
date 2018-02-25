874dcceece687812cdd4c6b3737c3b38564fd5ab
Caleb L. Power <clpower95@gmail.com>
2018-02-25 09:36:24 -0600

    initial commit

---
+++ src/org/okcrobot/scouter/MunkeeScouter.java
@@ -0,0 +1,9 @@
+package org.okcrobot.scouter;
+
+public class MunkeeScouter {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello, world!"); //exists to initialize repo
+  }
+  
+}
